1dfd70a3b8f84b7b033f7b9d523a60005399b410
Cmunny <connor-carpenter@comcast.net>
2018-04-19 12:10:03 -0700

    more things

--- Lab_4_Submission/dsilv022_ccarp006_lab4_part3.c
+++ Lab_4_Submission/dsilv022_ccarp006_lab4_part3.c
@@ -1,9 +1,13 @@
-enum SM_Christmas {SM_start, SM_wait,SM_invert} SM_var;
-unsigned char someVar = PINB;
-tickFct() {
+#include <avr/io.h>
+
+enum SM_Christmas {SM_start, SM_wait,SM_invert, SM_button_wait} SM_var;
+unsigned char someVar = 0;
+void tickFct() {
+	someVar = PINB;
     switch (SM_var) {
         case SM_start:
             SM_var = SM_wait;
+			PINB = 0x0A;
             break;
         case SM_wait:
             if (!(PINA & 0x01)) {
@@ -13,18 +17,32 @@ tickFct() {
             }
             break;
         case SM_invert:
-            SM_var = SM_wait;
+            SM_var = SM_button_wait;
             break;
+		case SM_button_wait: SM_var = !(PINA & 0x01) ? SM_button_wait : SM_wait;
+		break;
     }
     switch (SM_var) {
         case SM_wait:
             //do nothing
             break;
         case SM_invert:
-            PINB = ~(someVar);
+            PORTB = 0x00; PORTB = ~((someVar) & 0x0F);
             break;
         default:
             //do nothing
             break;
     }
+}
+
+int main(void)
+{
+	SM_var = SM_start;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+
+	while (1)
+	{
+		tickFct();		
+	}
 }
